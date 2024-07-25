6c57aed5a8a9ac689cd65c0870f95a2664a7b55b
yeong0211 <geunyeong0211@gmail.com>
2024-07-25 12:43:47 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1568 ms, Memory: 337204 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,25 +1,28 @@
 import java.io.*;
 import java.util.*;
 
-class Main
-{
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		int [] arr = new int [n];
-		for(int i=0;i<n;i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		br.close();
-		
-		// Collections.sort() : List가 메모리 사용이 크다 -> 메모리 초과
-		Arrays.sort(arr);
-		
-		for(int i : arr) {
-			bw.write(i+"\n");
-		}
-		bw.flush();
-		bw.close();
-	}
+class Main {
+    public static void main(String[] args) throws IOException    {
+    	
+    	int [] counting = new int[10001];
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	int n = Integer.parseInt(br.readLine());
+    	
+    	// Counting Sort
+    	// 시간제한이 촉박하며, 입력되는 수의 범위가 제한적이므로(0 <= x <= 10,000) Counting Sort를 사용한다. 
+        for(int i=0;i<n;i++) {
+        	counting[Integer.parseInt(br.readLine())]++;
+        }
+        for(int i=1;i<10001;i++) {
+        	while(counting[i]>0) {
+        		sb.append(i).append('\n');
+        		counting[i]--;
+        	}
+        }
+        
+    	System.out.println(sb);
+    	br.close();
+    }
 }
